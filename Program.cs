2a9bcee685b9469439bfe03dc6b64c22a0ba3bc7
janetuw <nijanet123@gmail.com>
2018-12-04 10:11:10 +0000

    aded index

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿sing system;
+using system.collections.generics;
+using system. linq;
+using system.text;
+using system .threading.tasks;
+namespace calc{}
+static void main(string[] args){
+    char choice;
+    var obj1=new Calculate();
+    console.writeline("enter first number");
+var num1=convert.ToDouble( Console.readline);
+console.writeline("enter the second number");
+var num2=convert.ToDouble(console.readline);
+console .readkey();
+choice=Console.readline();
+switch(grp){
+    case'+': System.Console.WriteLine(num1+num2);}
+    case'-': System.Console.WriteLine(num1-num2);
+case'*': System.Console.WriteLine(num1*num2);
+case'/': System.Console.WriteLine(num1/num2);
+default:System.Console.WriteLine("select any poperation")
+    
+}
+
+    
---
+++ calculate.cs
@@ -0,0 +1,29 @@
+namespace calculate
+static void main(){
+class Calculate{
+  public  double sum;
+    public double add(double num1, double num2){
+        
+        return (num1+num2);
+
+    }
+    
+        public double substract(double num1,double num2){
+return num1-num2;
+    }
+    public double Multiply(double num1,double num2){
+return num1*num2;
+    }
+    public double divide(double num1,double num2){
+return num1/num2;
+if(num2!=0){
+    return num1/num2;
+}
+    else
+    {
+        break;
+    }
+}
+    }
+}
+}
